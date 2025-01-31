388ad6ae7d4217af5e0fada4712589470d41a7c7
KubaLubiMaslo <czemek.jakub@gmail.com>
2025-01-31 23:39:13 +0100

    d

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/proxy", async (req, res) => {
